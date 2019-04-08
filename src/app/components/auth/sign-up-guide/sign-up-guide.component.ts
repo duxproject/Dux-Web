6651ef66fed373fa50b6e2c501282b6f844f1fbf
Lahiru mirihagoda <lahiru.mirihagoda@gmail.com>
2019-04-08 15:22:35 +0530

    Bug fix in auth

--- src/app/components/auth/sign-up-guide/sign-up-guide.component.ts
+++ src/app/components/auth/sign-up-guide/sign-up-guide.component.ts
@@ -11,10 +11,16 @@ import { AuthService } from "../../../shared/services/auth.service";
 export class SignUpGuideComponent implements OnInit {
 
 
-  constructor(public authService: AuthService) { }
-
+  constructor(
+    public authService: AuthService
+    ) { }
 
   ngOnInit() {
+  }
+
+  getUrl()
+  {
+    return "url('https://loveincorporated.blob.core.windows.net/contentimages/fullsize/8d311461-e6ec-4e5a-9eab-6a117a43f9c1-woman-travelling-alone.jpg')";
   }
 
 }
