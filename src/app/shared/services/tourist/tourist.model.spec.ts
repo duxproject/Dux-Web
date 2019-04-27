9229a7a012c9a1d1ca74ffe15ab1dfb2e03324f5
pasindusiriwardana <pasindulasithjk@gmail.com>
2019-04-27 23:01:31 +0530

    Tourist can create a new tour plan, in the firestore db

---
+++ src/app/shared/services/tourist/tourist.model.spec.ts
@@ -0,0 +1,7 @@
+import { Tourist } from './tourist.model';
+
+describe('Tourist', () => {
+  it('should create an instance', () => {
+    expect(new Tourist()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/services/tourist/tourist.model.ts
@@ -0,0 +1,12 @@
+export class Tourist {
+
+  id: string;
+  fullName: string;
+  country: string;
+  startDate: string;
+  duration: string;
+  startLoc: string;
+  endLoc: string;
+  budget: string;
+  
+}
