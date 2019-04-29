8a7abb01c8e3e4946613d7b5cc2cf845b97c378e
Lahiru Mirihagoda <lahiru.mirihagoda@gmail.com>
2019-04-29 20:09:04 +0530

    Update tour.component.ts

--- src/app/components/tourist/travel-plan/tour/tour.component.ts
+++ src/app/components/tourist/travel-plan/tour/tour.component.ts
@@ -10,7 +10,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class TourComponent implements OnInit {
 
-  constructor(private service: TouristService,
+  constructor(public service: TouristService,
     private firestore:AngularFirestore) { }
 
   ngOnInit() {
