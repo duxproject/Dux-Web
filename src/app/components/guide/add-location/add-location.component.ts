2313fc5dad872ee472297f33b73695af56487fe8
sanjanaherath2 <sanjanaherath2@github.com>
2019-08-14 11:30:42 +0530

    guide location add component completed

--- src/app/components/guide/add-location/add-location.component.ts
+++ src/app/components/guide/add-location/add-location.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../shared/services/auth.service';
+import { Location } from '../../../shared/services/location/location';
+import { LocationService } from '../../../shared/services/location/location.service';
+import { } from '../../admin/map/map.component';
+declare var ol: any;
 
 @Component({
   selector: 'app-add-location',
@@ -6,10 +11,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../../../assets/css/material-dashboard.min.css']
 })
 export class AddLocationComponent implements OnInit {
+  title = 'Dux-Web | Guide Dashboard';
 
-  constructor() { }
+
+  location: Location = {
+    description: '',
+    locationName: '',
+    loc: {
+      longitude: '',
+      latitude: ''
+    },
+    verified: true
+  };
+
+  constructor(public authService: AuthService, private locationService: LocationService) { }
+
 
   ngOnInit() {
+    const loca = JSON.parse(localStorage.getItem('loc'));
+    this.location.locationName = loca.address;
+    this.location.loc.longitude = loca.latlng.lng;
+    this.location.loc.latitude = loca.latlng.lat;
+  }
+
+  getname(): string {
+    const loc = JSON.parse(localStorage.getItem('loc'));
+    return loc.address;
+  }
+
+  getlat(): string {
+    const loc = JSON.parse(localStorage.getItem('loc'));
+    return loc.latlng.lat;
+  }
+
+  getlong(): string {
+    const loc = JSON.parse(localStorage.getItem('loc'));
+    return loc.latlng.lng;
+  }
+
+  onSubmit() {
+    const a = JSON.parse(localStorage.getItem('loc'));
+    if (a !== '' && this.location.description !== '') {
+      this.locationService.addLocation(this.location);
+      this.location.locationName = '';
+      this.location.description = '';
+      this.location.loc.latitude = '';
+      this.location.loc.longitude = '';
+    }
+    else {
+      console.log("Could not submit");
+    }
   }
 
 }
