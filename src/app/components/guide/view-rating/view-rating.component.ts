2ba7571f4e2b8f8087aaa7ba99fca43f78abe6d4
sanjanaherath2 <sanjanaherath2@github.com>
2019-08-16 09:22:10 +0530

    guide add rating function

--- src/app/components/guide/view-rating/view-rating.component.ts
+++ src/app/components/guide/view-rating/view-rating.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { StarService } from '../../../shared/services/star/star.service';
+import { Star } from '../../../shared/services/star/star';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view-rating',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewRatingComponent implements OnInit {
 
-  constructor() { }
+  title = "Dux | View Ratings";
+  id="";
+  stars: Observable<any>;
+  avgRating: Observable<any>;
+
+  constructor(private route: ActivatedRoute, public starService: StarService, public authService: AuthService) { }
+
+  getStr() {
+    const stars = JSON.parse(localStorage.getItem('stars'));
+    this.stars = stars; 
+  }
 
   ngOnInit() {
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    this.id = user.uid;
+
+    this.stars = this.starService.getStars(this.id)
+    this.avgRating = this.stars.pipe(map(arr => {
+      const ratings = arr.map(v => v.value)
+      return ratings.length ? ratings.reduce((total, val) => total + val) / arr.length : 'not reviewed'
+    }));
+
   }
+
 
 }
